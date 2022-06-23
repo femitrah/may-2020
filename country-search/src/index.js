c22523893a7b10ac19fd21f878d64ae55227a588
sivasankaran-mitrahsoft <sivasankaran.s@mitrahsoft.com>
2022-06-23 20:19:22 +0530

    react first exercise

---
+++ country-search/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+import './index.css';
+// import Test from'./App.test';
+// import Details from './index';
+
+
+
+ReactDOM.render(<App/>,document.getElementById("root"))
+
+// ReactDOM.render(<Style/>,document.getElementById("root"))
+
+
