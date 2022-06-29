45f1c5401d8ee3f02d8f4d72c65aabefd7c3fc23
abdulms01 <107870237+abdulms01@users.noreply.github.com>
2022-06-29 17:16:36 +0530

    Add files via upload

---
+++ ES-6  PART-1/Default values for parameters/1.function call/script.js
@@ -0,0 +1,10 @@
+function sum(a = 3, b = 5) {
+    return a + b;
+}
+console.log(sum(6, 9));
+console.log(sum(7));
+console.log(sum());
+
+
+
+
---
+++ ES-6  PART-1/arrow functions/4.arrow function to single multiline/script.js
@@ -0,0 +1,11 @@
+let print=()=>console.log("any statement")
+print()
+
+let add=(a,b)=>console.log(a+b)
+add(5,6)
+
+let concat=(firstString,secondString)=>console.log(firstString+secondString)
+ concat("a","b")
+
+ let checkleap=(params)=> leapYear(params)
+ checkleap(1995)
